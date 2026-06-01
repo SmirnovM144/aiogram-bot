db04e25cebc2857503f9f786387436548f3d63bc
Max Smirnov <makssabdal@gmail.com>
2026-06-01 12:38:04 +0300

    Create core module

---
+++ core/__init__.py
@@ -0,0 +1,10 @@
+"""
+Core module containing constants and shared configurations.
+"""
+from core.constants import CAKE_NAMES, SIZES, LOGISTICS
+
+__all__ = [
+    'CAKE_NAMES',
+    'SIZES',
+    'LOGISTICS',
+]
